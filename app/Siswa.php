c245ebd87f652a6d63e1d7a5105519fa56ca2e11
Rizal Fauzy <rizalfauzy@it.student.pens.ac.id>
2017-01-31 06:44:35 +0700

    Accessor Mutator
    Accessor Mutator for nama_siswa

--- app/Siswa.php
+++ app/Siswa.php
@@ -8,4 +8,12 @@ class Siswa extends Model
 {
     protected $table = 'siswa';
     protected $fillable = ['nisn', 'nama_siswa', 'tanggal_lahir', 'jenis_kelamin'];
+
+    public function getNamaSiswaAttribute($nama_siswa) {
+    	return ucwords($nama_siswa);
+    }
+
+    public function setNama_SiswaAttribute($nama_siswa) {
+    	return strtolower($nama_siswa);
+    }
 }
